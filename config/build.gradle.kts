c2d81dfae9f4c49961d2d276cacb576919bebb75
Hasanboy2003 <maxmudovhasanboy070@gmail.com>
2022-12-01 22:17:16 +0500

    moving fields to top

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.sonarqube").version("3.4.0.2513")
 }
 
 group = "com.epam.esm"
@@ -7,7 +8,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
 
+sonarqube {
+    properties {
+        property("sonar.projectName","RestApiBasics")
+        property("sonar.host.url","http://localhost:9000")
+        property("sonar.login","sqp_26731be87f68b24b256d6c8845854608f3e70445")
+    }
 }
 
 dependencies {
